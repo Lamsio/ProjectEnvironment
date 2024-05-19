d5c41dc81b9460913beb2062daa45d0b4be92e1b
TommyLam <tommylam@tommymail.cn>
2024-05-19 20:21:44 +0800

    v1.0

--- src/main/java/com/java/webmvc/mapper/GoodsMapper.java
+++ src/main/java/com/java/webmvc/mapper/GoodsMapper.java
@@ -4,8 +4,6 @@ import com.java.webmvc.pojo.Student;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
-import java.util.Map;
-
 @Repository
 public interface GoodsMapper {
 
